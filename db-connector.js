a0281acd900ca9bbfe99c01769d414c96d667c3a
rstoltzm-profile <86094615+rstoltzm-profile@users.noreply.github.com>
2021-12-21 12:58:16 -0700

    Update db-connector.js

--- db-connector.js
+++ db-connector.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
+  host            : '<REMOVE>',
   user            : '<REMOVE>',
   password        : '<REMOVE>',
   database        : '<REMOVE>',
